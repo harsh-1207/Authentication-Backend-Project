fe5e6e2bf7b6896ab073bc5bc853d0af40300b89
Harsh <harshbisht113322@gmail.com>
2025-09-10 09:40:23 +0530

    Email Message functionality

---
+++ src/main/java/in/harshbisht/authify/service/EmailService.java
@@ -0,0 +1,27 @@
+package in.harshbisht.authify.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    // takes the value set in application.properties
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to our platform");
+        message.setText("Hello " + name + ", " + "\n\n Thanks for registering with us!\n\nRegards, \nAuthify Team");
+        javaMailSender.send(message);
+    }
+}
